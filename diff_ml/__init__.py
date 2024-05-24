3c28733ef998d286fc4a98fd2e9ebbbbbfb21a0f
Neil Kichler <neil.kichler@gmail.com>
2024-05-24 14:36:32 +0200

    add smoothing to public API

--- diff_ml/__init__.py
+++ diff_ml/__init__.py
@@ -1,10 +1,10 @@
 import importlib.metadata
 
-from diff_ml import losses, nn, typing
+from diff_ml import losses, nn, smoothing, typing
 from diff_ml.ad import hmp, hvp
 from diff_ml.nn import train
 
 
-__all__ = ["hmp", "hvp", "train", "losses", "nn", "typing"]
+__all__ = ["hmp", "hvp", "train", "losses", "nn", "smoothing", "typing"]
 
 __version__ = importlib.metadata.version("diff-ml")
