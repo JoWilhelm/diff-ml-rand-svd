b47df6b2c52e4339323bbab9f4e3ff5aded3f20c
Neil Kichler <neil.kichler@gmail.com>
2024-05-24 15:08:41 +0200

    minor cleanup: remove unused imports

--- diff_ml/typing.py
+++ diff_ml/typing.py
@@ -1,13 +1,11 @@
 from collections.abc import Callable, Generator
-from typing import Optional
 from typing_extensions import TypeAlias
 
-from jaxtyping import Array, Float, PRNGKeyArray
+from jaxtyping import Array, Float
 
 
 Data: TypeAlias = dict[str, Float[Array, "n_samples ..."]]
 
 DataGenerator: TypeAlias = Generator[Data, None, None]
 
-# Model: TypeAlias = Callable[[Array, Optional[PRNGKeyArray]], Array]
 Model: TypeAlias = Callable
